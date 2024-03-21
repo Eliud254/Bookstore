30797cced3ae58dd3bf98026104688eb8ff70bca
eliud <eliudobure@gmail.com>
2024-03-21 17:25:21 +0300

    Second commit

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
 
 const Hero = () => {
   return (
-    <div>Hero slider</div>
+    <section className='mt-3 relative overflow-hidden text-white'>
+      <div className='container max-w-screen-xl mx-auto'>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      </div>
+    </section>
   )
 }
 
